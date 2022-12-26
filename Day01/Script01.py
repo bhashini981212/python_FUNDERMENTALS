e5b4b08cd303ad5f676759256499e9c2386760be
bhashini981212 <bhashinibaiz@gmail.com>
2022-12-26 20:05:14 +0530

    Learnt Print function

---
+++ Day01/Script01.py
@@ -0,0 +1,15 @@
+#basic print command
+print("Hello World")
+
+#terminal commands -
+
+#single line comments
+
+"""
+terminal commands
+ls_ view files
+cd-change directry
+python<filename>.py - run a python file name
+"""
+print("Hello I'am Bhashini")
+
