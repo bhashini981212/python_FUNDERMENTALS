84f35fd873a021c16e4262afb3a2d95f8588310a
bhashini981212 <bhashinibaiz@gmail.com>
2022-12-26 20:34:04 +0530

    Exercise using print function

---
+++ Day01/exercise01.py
@@ -0,0 +1,21 @@
+#create small program with following requirements
+name=input("Enter your full name:")
+age=input("Enter your age:")
+contact=input("Enter your contact number:")
+print ("Hi I'm "+name+".I am "+ age+ "years old. My contact number is"+ contact+".")
+# Create a small program for following requirements
+"""
+    first ask the user's name in the program 
+    then ask the age of the user
+    last ask the mobile number of the user
+
+    finally print the given input as follows
+
+    Hi I'm <name>. 
+    I am <age>. 
+    My contact number is <phoneNumber>.
+
+"""
+#variable convensions
+
+
