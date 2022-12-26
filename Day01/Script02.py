91463442ae0cbc50cdda6f09f68e1f14c9f22a7f
bhashini981212 <bhashinibaiz@gmail.com>
2022-12-26 20:13:52 +0530

    Learnt Strings

---
+++ Day01/Script02.py
@@ -0,0 +1,20 @@
+#write small discription about your self
+
+print("Hello I'am Bhashini.")
+print("I'am 24 years old.")
+print("I am 4th year university student.")
+
+print('''Hello I'am Bhashini.
+I'am 24 years old.
+I am 4th year university student.''')
+print()
+print("Hello I'am Bhashini\n I'am 24 years old.\n I am 4th year university student.")
+
+#string operatios
+#concatenations
+
+
+print("Hello "+"world"*9)
+
+name= input("enter your name:")
+
