8a9b40e21dedc53cbb0bd8068d7215f57f1c8709
bhashini981212 <bhashinibaiz@gmail.com>
2022-12-26 20:14:50 +0530

    Learnt variables with strings

---
+++ Day01/Script03.py
@@ -0,0 +1,15 @@
+#defining a variable
+name="bhashini"
+print(name)
+name="promodya"
+print(name)
+
+
+#leaving an empty line
+
+#using variables with strings
+print ("my name is "+ name)
+print ("my name is ",name)
+print ("my name is {} ".format(name))
+print("My name is {}".format(name))
+print(f"My name is {name}")
