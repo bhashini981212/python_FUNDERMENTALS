421c6ceb8b33bc8ff6842d0c5f67c4bbbf5101dd
bhashini981212 <bhashinibaiz@gmail.com>
2022-12-26 20:31:14 +0530

    learn arithmetic operations

---
+++ Day02/Script01.py
@@ -0,0 +1,23 @@
+Operend1= 34
+Operend2= 54
+
+answer= Operend1 + Operend2
+print("Additon  is",answer)
+
+answer= Operend1 - Operend2
+print("Substraction  is",answer)
+
+answer= Operend1 * Operend2
+print("Multiplication   is",answer)
+
+answer= Operend1 ** 3
+print("Cube of operent1  is",answer)
+
+answer= Operend2 ** 3
+print("Cube of operent2  is",answer)
+
+answer= Operend1 ** 0.5
+print("Squreroot of operent1  is",answer)
+
+answer= Operend2 ** 0.5
+print("Squre root of operent2  is",answer)
